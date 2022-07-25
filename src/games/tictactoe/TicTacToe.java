e31da4a5fb689f16fbb75cd851f5ccb828f18075
shivani-guduru <gudurushivani111@gmail.com>
2022-07-25 12:05:05 +0530

    changeNumber

--- src/games/tictactoe/TicTacToe.java
+++ src/games/tictactoe/TicTacToe.java
@@ -3,7 +3,7 @@ package games.tictactoe;
 public class TicTacToe {
     private char[][] board = {
             {'1', '2', '3'},
-            {'4', '5', '6'},
+            {'4', '0', '6'},
             {'7', '8', '9'}
     };
 
