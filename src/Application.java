59f23372ca4af4a2796efe340a7b9b356775ba7d
Pranathi-zensark <109594629+Pranathi-zensark@users.noreply.github.com>
2022-08-04 15:31:08 +0530

    Update Application.java
    Assignment 2

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,20 @@
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        GetterSetterDemo demo = new GetterSetterDemo();
-        demo.setName("Hello World");
-        System.out.println(demo.getName());
+        int totalgames = 0;
+        while(true) {
+            GuessTheNumber game = new GuessTheNumber();
+            game.run();
+            System.out.println("To continue the game enter y/n ");
+            Scanner scanner = new Scanner(System.in);
+            char exit = scanner.next().charAt(0);
+            totalgames++;
+            if (exit == "n".charAt(0)) {
+                break;
+            }
+        }
+        System.out.println("Result: you have win in " + GuessTheNumber.wins + " game(s) you have lossed in "+ GuessTheNumber.loss+ " game(s) out total played game(s) is "+totalgames);
     }
 }
