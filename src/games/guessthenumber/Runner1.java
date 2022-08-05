4f4d5660f3bbec100de25e99ce164b5130f6bbe0
Sindhu-kande <kandesindhu1312@gmail.com>
2022-08-05 12:17:49 +0530

    code
     guess the number code

---
+++ src/games/guessthenumber/Runner1.java
@@ -0,0 +1,8 @@
+import games.guessthenumber.guesstheNumber;
+
+public class Runner1 {
+    public static void main(String[] args) {
+        guesstheNumber game = new guesstheNumber();
+        game.run();
+    }
+}
